356ae04823b00f7e410e56e618e28d9f5fdc5b11
GeorgeLimaDev <george.lima@vtex.com.br>
2025-03-09 12:03:59 -0300

    Bugfix: RMI e anotações

--- src/main/java/clinix/com/clinicaservice/repository/ClinicaRepository.java
+++ src/main/java/clinix/com/clinicaservice/repository/ClinicaRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import clinix.com.clinicaservice.model.Clinica;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClinicaRepository extends JpaRepository<Clinica,Long> {
 
     Optional<Clinica> findByNomeFantasia(String nomeFantasia);
